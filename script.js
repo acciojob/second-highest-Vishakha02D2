208b33db6521a3434d3767092c4180469f1f8860
Vishakha02D2 <120273836+Vishakha02D2@users.noreply.github.com>
2023-06-30 22:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+function secondHighest(arr) {
+  // Check if arr is empty or has only 1 element
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  // Find the highest element
+  let highest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > highest) {
+      highest = arr[i];
+    }
+  }
+
+  // Find the second highest element
+  let secondHighest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== highest && arr[i] > secondHighest) {
+      secondHighest = arr[i];
+    }
+  }
+
+  // Check if all elements are the same
+  if (secondHighest === -Infinity) {
+    return -Infinity;
+  }
+
+  return secondHighest;
+}
 //your JS code here. If required.
+function secondHighest(arr) {
+  // Check if arr is empty or has only 1 element
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  // Find the highest element
+  let highest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > highest) {
+      highest = arr[i];
+    }
+  }
+
+  // Find the second highest element
+  let secondHighest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== highest && arr[i] > secondHighest) {
+      secondHighest = arr[i];
+    }
+  }
+
+  // Check if all elements are the same
+  if (secondHighest === -Infinity) {
+    return -Infinity;
+  }
+
+  return secondHighest;
+}
